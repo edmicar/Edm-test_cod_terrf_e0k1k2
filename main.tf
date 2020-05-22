12cd82f1ccba8b4d0b322399be944b6c16e7221c
root <edmilsoncar.oli@gmail.com>
2020-05-22 19:58:36 +0000

    eks - create 2205 - v1 - Sem cred

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-module "eks-node-wrk" {
-  source             = "./aws-modules/eks-node-wrk/aws"
-  cluster_name       = var.cluster-name
+module "eks" {
+  source                   = "./aws-modules/eks/aws"
+  cluster_name             = var.cluster-name
   subnets = var.vpc_subnets
 
-    map_roles = [
+  map_roles = [
     {
       rolearn  = "arn:aws:iam::599853486601:role/Role_Eksterraform"
       username = "Role_Eksterraform"
       groups   = ["system:masters"]
     },
   ]
-}
+  }
 
